ea55eb06515853c0571c67b770005caf558b5ef3
Dusan Todorovic <dtodorovic40@gmail.com>
2023-01-26 23:08:05 +0100

    feat: create function to save/get workshop img

--- backend/src/imager.ts
+++ backend/src/imager.ts
@@ -1,5 +1,5 @@
 const base_user_directory = './public/users/'
-
+const base_workshop_directory = './public/workshops/'
 function extension_from_char(type) {
   if (type == '/') return ".jpg"
   if (type == 'i') return ".png"
@@ -19,6 +19,40 @@ export function base64_to_image(base64Raw, username) {
   }
 
   FileSystem.writeFile(base_user_directory + username + extension_from_char(base64Raw.charAt(0)), base64Raw, 'base64', function (err) { });
+}
+
+export function main_image_to_base64(name) {
+  const FileSystem = require("fs");
+
+  var bitmap = null;
+
+  try {
+    bitmap = FileSystem.readFileSync(base_workshop_directory + name + "/1" + ".jpg");
+  } catch {
+    bitmap = null;
+  }
+  
+  if (!bitmap) {
+    try {
+      bitmap = FileSystem.readFileSync(base_workshop_directory + name + "/1" + ".png");
+    } catch {
+      bitmap = null;
+    }
+    
+    if (!bitmap) {
+      try {
+        bitmap = FileSystem.readFileSync(base_workshop_directory + name + "/1" + ".webp");
+      } catch {
+        bitmap = null;
+      }
+    } 
+  } 
+
+  if (!bitmap) {
+    return null;
+  }
+
+  return Buffer.from(bitmap).toString('base64');
 }
 
 export function image_to_base64(username) {
@@ -51,6 +85,7 @@ export function image_to_base64(username) {
   if (!bitmap) {
     return null;
   }
+  
   return Buffer.from(bitmap).toString('base64');
 }
 
