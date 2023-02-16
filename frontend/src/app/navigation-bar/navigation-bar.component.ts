95c03bb75621e0ba58919fbebbe8042a66b65b5b
Dusan Todorovic <dtodorovic40@gmail.com>
2023-02-16 21:23:03 +0100

    feat: navigation bar login/reg

--- frontend/src/app/navigation-bar/navigation-bar.component.ts
+++ frontend/src/app/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-navigation-bar',
@@ -7,64 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationBarComponent implements OnInit {
 
-  constructor() { }
-  ngOnInit(): void {
+  user : User;
+
+  constructor(private router: Router) { 
+  }
+  
+  ngOnInit(): void { 
+    this.user  = JSON.parse(localStorage.getItem("user"));
   }
 
-  imageError: string;
-  isImageSaved: boolean;
-  cardImageBase64: string;
-
-  processFile(fileInput: any) {
-    this.imageError = null;
-    if (fileInput.target.files && fileInput.target.files[0]) {
-      // Size Filter Bytes
-      const max_size = 20971520;
-      const allowed_types = ['image/png', 'image/jpeg'];
-      const max_height = 300;
-      const max_width = 300;
-
-      if (fileInput.target.files[0].size > max_size) {
-        this.imageError =
-          'Maximum size allowed is ' + max_size / 1000 + 'Mb';
-
-        return false;
-      }
-      const reader = new FileReader();
-      reader.onload = (e: any) => {
-        const image = new Image();
-        image.src = e.target.result;
-        image.onload = (rs) => {
-          const img_height = rs.currentTarget['height'];
-          const img_width = rs.currentTarget['width'];
-
-          console.log(img_height, img_width);
-
-
-          if (img_height > max_height && img_width > max_width) {
-            this.imageError =
-              'Maximum dimentions allowed ' +
-              max_height +
-              '*' +
-              max_width +
-              'px';
-            return false;
-          } else {
-            const imgBase64Path = e.target.result;
-            this.cardImageBase64 = imgBase64Path;
-            this.isImageSaved = true;
-            // this.previewImagePath = imgBase64Path;
-          }
-          console.log(this.cardImageBase64)
-          return true;
-        };
-      };
-
-      reader.readAsDataURL(fileInput.target.files[0]);
-
-    }
-
-    return true;
+  login() {
+    this.router.navigate(['login']);
   }
 
+  register() {
+    this.router.navigate(["register"]);
+  }
+
+  logout() {
+    localStorage.clear();
+    this.user = null;
+  }
 }
